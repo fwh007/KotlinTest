6af04a761ddef299ad1538b722a796280bcd6bc7
Winter <gzfengwenhao@corp.netease.com>
2017-06-22 18:18:39 +0800

    添加新闻类New

---
+++ app/src/main/java/com/github/fwh007/kotlintest/New.kt
@@ -0,0 +1,14 @@
+package com.github.fwh007.kotlintest
+
+/**
+ * TODO 类说明
+ *
+ * @author : Winter
+ */
+data class New(
+        val title: String,
+        val abstract: String,
+        val source: String,
+        val image_url: String,
+        val item_seo_url: String
+)
---
+++ app/src/main/java/com/github/fwh007/kotlintest/NewViewModel.kt
@@ -0,0 +1,13 @@
+package com.github.fwh007.kotlintest
+
+import android.arch.lifecycle.ViewModel
+
+/**
+ * TODO 类说明
+ *
+ * @author : Winter
+ */
+class NewViewModel(
+        val newId: String,
+        val aNew: New
+) : ViewModel()
